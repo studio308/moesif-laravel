431cb7bf99c996f6f43a5c6bad8cbcc1ce49c0de
Jonny Pickett <jonathanpickett@mac.com>
2018-07-10 18:25:22 -0700

    Create Moesif logic

---
+++ src/JonnyPickett/MoesifLaravel/Middleware/Moesif.php
@@ -0,0 +1,189 @@
+<?php namespace JonnyPickett\MoesifLaravel\Middleware;
+
+use Config;
+use DateTime;
+use DateTimeZone;
+use JonnyPickett\MoesifLaravel\Sender\MoesifApi;
+use Log;
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+class Moesif implements HttpKernelInterface
+{
+
+    /**
+     * The wrapped kernel implementation.
+     *
+     * @var \Symfony\Component\HttpKernel\HttpKernelInterface
+     */
+    protected $app;
+
+    /**
+     * Create a new Moesif instance.
+     *
+     * @param  \Symfony\Component\HttpKernel\HttpKernelInterface  $app
+     * @return void
+     */
+    public function __construct(HttpKernelInterface $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Handle the given request and get the response.
+     *
+     * @implements HttpKernelInterface::handle
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @param  int   $type
+     * @param  bool  $catch
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(SymfonyRequest $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true)
+    {
+        // do action before response
+        $t = microtime(true);
+        $micro = sprintf("%06d", ($t - floor($t)) * 1000000);
+        $startDateTime = new DateTime(date('Y-m-d H:i:s.'.$micro, $t));
+        $startDateTime->setTimezone(new DateTimeZone("UTC"));
+
+        // Handle on passed down request
+        $response = $this->app->handle($request, $type, $catch);
+
+        $applicationId = Config::get('moesif::config.application_id');
+        $apiVersion = Config::get('moesif::config.apiVersion');
+        $maskRequestHeaders = Config::get('moesif::config.maskRequestHeaders');
+        $maskRequestBody = Config::get('moesif::config.maskRequestBody');
+        $maskResponseHeaders = Config::get('moesif::config.maskResponseHeaders');
+        $maskResponseBody = Config::get('moesif::config.maskResponseBody');
+        $identifyUserId = Config::get('moesif::config.identifyUserId');
+        $identifySessionId = Config::get('moesif::config.identifySessionId');
+        $getMetadata = Config::get('moesif::config.getMetadata');
+        $skip = Config::get('moesif::config.skip');
+        $debug = Config::get('moesif::config.debug');
+        if (is_null($debug)) {
+            $debug = false;
+        }
+        // if skip is defined, invoke skip function.
+        if (!is_null($skip)) {
+            if ($skip($request, $response)) {
+                if ($debug) {
+                    Log::info('[Moesif] : skip function returned true, so skipping this event.');
+                }
+                return $response;
+            }
+        }
+        if (is_null($applicationId)) {
+            throw new Exception('Moesif application_id is missing. Please provide application_id in package config file.');
+        }
+        $requestData = [
+            'time' => $startDateTime->format('Y-m-d\TH:i:s.uP'),
+            'verb' => $request->method(),
+            'uri' => $request->fullUrl(),
+            'ip_address' => $request->ip()
+        ];
+        if (!is_null($apiVersion)) {
+            $requestData['api_version'] = $apiVersion;
+        }
+        $requestHeaders = [];
+        foreach ($request->headers->keys() as $key) {
+            $requestHeaders[$key] = (string) $request->headers->get($key);
+        }
+        if (!is_null($maskRequestHeaders)) {
+            $requestData['headers'] = $maskRequestHeaders($requestHeaders);
+        } else {
+            $requestData['headers'] = $requestHeaders;
+        }
+        $requestContent = $request->getContent();
+        if (!is_null($requestContent)) {
+            $requestBody = json_decode($requestContent, true);
+            if (is_null($requestBody)) {
+                if ($debug) {
+                    Log::info('[Moesif] : request body not be empty and not json, base 64 encode');
+                }
+                $requestData['body'] = base64_encode($requestContent);
+                $requestData['transfer_encoding'] = 'base64';
+            } else {
+                if (!is_null($maskRequestBody)) {
+                    $requestData['body'] = $maskRequestBody($requestBody);
+                } else {
+                    $requestData['body'] = $requestBody;
+                }
+            }
+        }
+        $endTime = microTime(true);
+        $micro = sprintf("%06d", ($endTime - floor($endTime)) * 1000000);
+        $endDateTime = new DateTime(date('Y-m-d H:i:s.'.$micro, $endTime));
+        $endDateTime->setTimezone(new DateTimeZone("UTC"));
+        $responseData = [
+            'time' => $endDateTime->format('Y-m-d\TH:i:s.uP'),
+            'status' => $response->status()
+        ];
+        $responseContent = $response->content();
+        if (!is_null($responseContent)) {
+            $jsonBody = json_decode($response->content(), true);
+            if (!is_null($jsonBody)) {
+                if (!is_null($maskResponseBody)) {
+                    $responseData['body'] = $maskResponseBody($jsonBody);
+                } else {
+                    $responseData['body'] = $jsonBody;
+                }
+            } else {
+                if (!empty($responseContent)) {
+                    $responseData['body'] = base64_encode($responseContent);
+                    $responseData['transfer_encoding'] = 'base64';
+                }
+            }
+        }
+        $responseHeaders = [];
+        foreach ($response->headers->keys() as $key) {
+            $responseHeaders[$key] = (string) $response->headers->get($key);
+        }
+        if (!is_null($maskResponseHeaders)) {
+            $responseData['headers'] = $maskResponseHeaders($responseHeaders);
+        } else {
+            $responseData['headers'] = $responseHeaders;
+        }
+        $data = [
+            'request' => $requestData,
+            'response' => $responseData
+        ];
+        $user = $request->user();
+        if (!is_null($identifyUserId)) {
+            $data['user_id'] = $this->ensureString($identifyUserId($request, $response));
+        } elseif (!is_null($user)) {
+            $data['user_id'] = $this->ensureString($user['id']);
+        }
+        if (!is_null($identifySessionId)) {
+            $data['session_token'] = $this->ensureString($identifySessionId($request, $response));
+        } elseif ($request->hasSession()) {
+            $data['session_token'] = $this->ensureString($request->session()->getId());
+        }
+        if (!is_null($getMetadata)) {
+            $data['metadata'] = $getMetadata($request, $response);
+        }
+        $moesifApi = MoesifApi::getInstance($applicationId, [
+            'fork' => true,
+            'debug' => $debug,
+        ]);
+        $moesifApi->track($data);
+
+        return $response;
+    }
+
+    /**
+     * @param $item
+     *
+     * @return string
+     */
+    protected function ensureString($item)
+    {
+        if (is_null($item)) {
+            return $item;
+        }
+        if (is_string($item)) {
+            return $item;
+        }
+        return strval($item);
+    }
+}
---
+++ src/config/config.php
@@ -0,0 +1,165 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Moesif Application ID
+    |--------------------------------------------------------------------------
+    |
+    | This is the Moesif application id.
+    |
+    */
+
+    'application_id' => getEnv('MOESIF_APPLICATION_ID'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Skip
+    |--------------------------------------------------------------------------
+    |
+    | Return true if the event is to be skipped.
+    |
+    */
+
+    'skip' => function ($request, $response) {
+        return false;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mask Request Headers
+    |--------------------------------------------------------------------------
+    |
+    | Add or remove request headers.
+    |
+    */
+
+    'maskRequestHeaders' => function ($headers) {
+        return $headers;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mask Request Body
+    |--------------------------------------------------------------------------
+    |
+    | Remove any fields from body that you don't want sent to Moesif.
+    |
+    */
+
+    'maskRequestBody' => function ($body) {
+        return $body;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mask Response Headers
+    |--------------------------------------------------------------------------
+    |
+    | Add or remove response headers.
+    |
+    */
+
+    'maskResponseHeaders' => function ($headers) {
+        return $headers;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mask Response Body
+    |--------------------------------------------------------------------------
+    |
+    | Remove any fields from body that you don't want sent to Moesif.
+    |
+    */
+
+    'maskResponseBody' => function ($body) {
+        return $body;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Identify User ID
+    |--------------------------------------------------------------------------
+    |
+    | Identify the user.
+    |
+    */
+
+    'identifyUserId' => function ($request, $response) {
+        if (is_null($request->user())) {
+            return null;
+        } else {
+            $user = $request->user();
+            return $user['id'];
+        }
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Identify Session ID
+    |--------------------------------------------------------------------------
+    |
+    | Identify the session.
+    |
+    */
+
+    'identifySessionId' => function ($request, $response) {
+        if ($request->hasSession()) {
+            return $request->session()->getId();
+        } else {
+            return null;
+        }
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Meta Data
+    |--------------------------------------------------------------------------
+    |
+    | Add any extra data to be sent to Moesif.
+    |
+    */
+
+    'getMetaData' => function ($request, $response) {
+        return [];
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Tags
+    |--------------------------------------------------------------------------
+    |
+    | Add any tags to be sent to Moesif.
+    |
+    */
+
+    'addTags' => function ($request, $response) {
+        return '';
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Version
+    |--------------------------------------------------------------------------
+    |
+    | Identify the API Version.
+    |
+    */
+
+    'apiVersion' => function ($request, $response) {
+        return null;
+    },
+
+    /*
+    |--------------------------------------------------------------------------
+    | Debug
+    |--------------------------------------------------------------------------
+    |
+    | Set to true to see debug information.
+    |
+    */
+
+    'debug' => true,
+);
